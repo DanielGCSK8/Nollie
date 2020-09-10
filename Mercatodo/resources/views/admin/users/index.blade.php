1121fa5d8e2c0e6fcfd69329eaa0118e08d29132
Danielrvrt <39166172+Danielrvrt@users.noreply.github.com>
2020-09-10 01:15:23 -0500

    Cambios en la estética de las vistas con tablas, usando sass

--- Mercatodo/resources/views/admin/users/index.blade.php
+++ Mercatodo/resources/views/admin/users/index.blade.php
@@ -2,72 +2,70 @@
 @section('content')
 
 
-        <div class="col">
-            <h3 style="font-family: Times New Roman">Usuarios</h3>
-        </div>
+            <div class="container">
+              <div class="panel panel-default">
+                  <div class="panel-heading">
+                    <h3 style="font-family: Times New Roman">Usuarios</h3>
+                  </div>
+                  <div class="panel-body">
+                      <table class="table-latitude">
+                           
+                            <thead>
+                              <th>Nombre</th>
+                              <th>Rol</th>
+                              <th>Celular</th>
+                              <th>Email</th>
+                              <th>Creado</th>
+                              <th>Actualizado</th>
+                              <th>Opciones</th>
+                          </thead>
+                          <tfoot>
+                              <tr>
+                                  <td colspan=3> <a href="/home"><button type="submit" name="button" class="btn btn-success">Volver</button> </a></td>
+                              </tr>
+                          </tfoot>
+                          <tbody>
 
-        <br>
+                            @if($users)
+                              @foreach ($users as $user)
 
-          <div class="row">
-                <div class="col">
-                <table class="table">
-            
-              <tr>
-                  <th>Nombre</th>
-                  <th>Rol</th>
-                  <th>Celular</th>
-                  <th>Email</th>
-                  <th>Creado</th>
-                  <th>Actualizado</th>
-                  <th>Opciones</th>
-              </tr>
-            @if($users)
-                @foreach ($users as $user)
+                              <tr>
+                                  <th>{{$user->name}}</th>
+                                  <td>{{$user->role->role}}</td>
+                                  <td>{{$user->cellphone}}</td>
+                                  <td>{{$user->email}}</td>
+                                  <td>{{$user->created_at}}</td>
+                                  <td>{{$user->updated_at}}</td>
+                                  <td>
+                                    <center>
+                                    <a href="{{route('users.edit', $user->id)}}"> <i class="fas fa-user-edit"></i></a>
+
+                                  </td>
+                                  <td>
+                                    
+                                    @if(!$user->trashed())
+                                    <form action="{{route('users.destroy', $user->id)}}" method="POST">
+                                      @method('DELETE')
+                                      @csrf
+                                    <button type="submit" class="btn btn-default ml-2">Inhabilitar</button>
+                                  </form>
+                                  @endif
                     
-                
-              <tr>
-              <td>{{$user->name}}</td>
-              <td>{{$user->role->role}}</td>
-              <td>{{$user->cellphone}}</td>
-              <td>{{$user->email}}</td>
-              <td>{{$user->created_at}}</td>
-              <td>{{$user->updated_at}}</td>
-              <td>
-                <a href="{{route('users.edit', $user->id)}}"> <button type="button" class="btn btn-primary">Editar</button>  </a>
-              </td>
-            
-              <td>
-                @if(!$user->trashed())
-                <form action="{{route('users.destroy', $user->id)}}" method="POST">
-                  @method('DELETE')
-                  @csrf
-                <button type="submit" class="btn btn-primary">Inhabilitar</button>
-              </form>
-              @endif
-
-              @if($user->trashed())
-              <form action="{{route('users.restore', $user->id)}}" method="POST">
-                @method('PUT')
-                @csrf
-              <input type="submit" class="btn btn-default" value="Habilitar"/></td>
-              @endif
-
-              </tr>
-              @endforeach
-            @endif
-              
-
-            </table>
-            <div class="col-md-12">
-              <div class="form-group">
-                  <a href="/home"><button type="submit" name="button" class="btn btn-success">Volver</button> </a>
+                                  @if($user->trashed())
+                                  <form action="{{route('users.restore', $user->id)}}" method="POST">
+                                    @method('PUT')
+                                    @csrf
+                                  <button type="submit" class="btn btn-default ml-2">Habilitar</td>
+                                  @endif
+                                  </td>
+                              </tr>
+                              
+                              @endforeach
+                            @endif
+                          </tbody>
+                      </table>
+                  </div>
               </div>
           </div>
-                </div>
-            </div>
-
-                        
-
-
 
 @endsection
